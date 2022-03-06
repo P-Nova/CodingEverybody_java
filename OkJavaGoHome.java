ea5b807e986ed7eed728bc4431872acb069c882b
76CD <100562275+P-Nova@users.noreply.github.com>
2022-03-07 06:39:31 +0900

    생활코딩_자바입문

---
+++ OkJavaGoHome.java
@@ -0,0 +1,30 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class OkJavaGoHome {
+
+	public static void main(String[] args) {
+		
+		String id = "JAVA APT 507";
+		
+		// Elevator call
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		// Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+	
+		// Light on
+		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting(id+" / floorLamp");
+		floorLamp.on();
+
+// Elevator 라는 '데이터 타입'의 '변수' myElevator 
+		
+	}
+
+}
---
+++ program.java
@@ -0,0 +1,12 @@
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+// 순차적으로 진행, 인간이 하고자 하는 일을 기계에게 위임, 자동화
+	}
+
+}
